e55cf7236eb0048ff9b7bc779423a50285e81e19
Camila do Amaral Ferreira <camila.2023301840@aluno.iffar.edu.br>
2025-06-09 14:26:41 -0300

    Update ex1_caneta.php

--- blanhh/ex1_caneta.php
+++ blanhh/ex1_caneta.php
@@ -1,7 +1,7 @@
 <?php
-//Definindo a classe caneta
+
 class caneta{
-    //definir atributos
+   
     public $cor;
 
     public $marca;
@@ -12,7 +12,7 @@ class caneta{
 
     public $carga;
 
-    //definir métodos
+  
 
     public function escrever() {
         echo"escrevendo...<br>";
@@ -30,23 +30,20 @@ class caneta{
 
 }
 
-//Instanciando a classe caneta
+
 
 $caneta1= new caneta();
-//Como os atributos da classe cnaeta são publicos, podemos acessa-los diretamente
 $caneta1->cor ="azul";
 $caneta1->marca = "BIC";
 $caneta1->ponta = 0.5;          
 $caneta1->tamanho = "grande";
 $caneta1->carga = 100;
-//Exibindo os atributos da caneta
 echo "Cor: " . $caneta1->cor ."<br>";
 echo "Marca: " . $caneta1->marca ."<br>";
 echo "Ponta: ". $caneta1->ponta ."<br>";
 echo "Tamanho: ". $caneta1->tamanho ."<br>";
 echo "Carga: ". $caneta1->carga ."<br>";
 
-//Chamando os métodos da caneta
 $caneta1->escrever();
 $caneta1->rabiscar();
 $caneta1->sublinhar();
